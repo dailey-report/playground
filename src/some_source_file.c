f42129c82cb023e215cccc45947603f14795869e
Michael J. Dailey <mdailey@eagleeyenetworks.com>
2018-11-28 11:53:29 -0600

    add some source

---
+++ src/some_source_file.c
@@ -0,0 +1,42 @@
+/**
+ * Mysterious function that was written 30+ years ago.
+ *
+ * ######################################
+ *
+ *                  __
+ *                  \/
+ *                __  __
+ *                \/  \/
+ *
+ *
+ * ######################################
+ * DANGER!  RADIATION PROTECTION REQUIRED
+ * ######################################
+ *
+ * Original author unknown, purpose unknown.
+ *
+ */
+#ifndef __SOME_SOURCE_PLGD
+#define __SOME_SOURCE_PLGD
+
+// 3rd party libraries
+#include <glib.h>
+#include <gio/gio.h>
+#include <signal.h>
+#include <czmq.h>
+
+// operational
+#include "cleaner.h"
+#include "playground_log.h"
+#include "playground_config.h"
+#include "playground_threads.h"
+#include "ipcoms.h"
+#include "file_interface.h"
+
+#endif
+
+void
+synced_thread_start()
+{
+  g_debug(G_STRLOC " if this was implemented, synced threads would be starting now");
+}
